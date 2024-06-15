5caf4dd753ca2d535e77fbf2b9e5d4882a27f17a
Dunconius <duncanbean84@gmail.com>
2024-06-16 07:04:37 +1000

    feat: app.js basic structure implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import './styles/App.css';
+import React from "react";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      
+    <div className="portfolio-site">
+      <nav className="anchor-menu">
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="contact">Contact</a></li>
+        </ul>
+      </nav>
+      <div className="main-content">
+        <About />
+        <Projects />
+        <Skills />
+        <Experience />
+        <Contact />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
