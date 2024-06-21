d1faaa18428c6db08972acdf5e0e06b67363cc22
Dunconius <duncanbean84@gmail.com>
2024-06-21 18:57:55 +1000

    feat: added about text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,27 @@ const About = () => (
 
   <section className="main-info" id="about">
     <h2>About Me</h2>
-    <p>As a junior web developer with a unique background, I bring a distinct perspective to web development. My decade-long experience in interior design and has given me an expertise for making user-centric designs which informs my approach to all things UX/UI. I bring a unique blend of creative and technical ability to everything I do. I have a passion for problem-solving and the technical side of any creative project.</p>
-    <p>I aspire to leverage my diverse design and technical background to create innovative, user-centric web solutions that enhance the digital experience and am eager to contribute to innovative teams and dynamic projects.</p>
+    <h4>Junior Web Developer</h4>
+    <div>
+      <p>As a junior web developer with a unique background, I bring a distinct perspective to web development. My decade-long experience in interior design and has given me an expertise for making user-centric designs which informs my approach to all things UX/UI. I bring a unique blend of creative and technical ability to everything I do. I have a passion for problem-solving and the technical side of any creative project.</p>
+    </div>
+    <div>
+      <p>I aspire to leverage my diverse design and technical background to create innovative, user-centric web solutions that enhance the digital experience and am eager to contribute to innovative teams and dynamic projects.</p>
+    </div>
+    <div>
+      <p>I am a budding web developer, eager to dive into the world of creating amazing online experiences. With a hunger for learning and a keen eye for detail, I'm here to share the web projects I've been working on as I grow my skills.</p>
+    </div>
+    <div>
+      <p>In my previous careers I have been an iterior designer, teacher, and 3D digital artist.</p>
+    </div>
+    <div>
+      <p>In 2015-16 I was studying 3D modelling and animation at AIE. During this time I did a deep dive into character rigging and taught myself how to use Python within Maya for this purposed. It was great fun writing code to build machines that did the tedious work for me. My high point was creating an automated character rigging interface that built skeletons and applied control rigs with the push of a button.</p>
+    </div>
+    <div>
+      <p>Over the past few years I have been dabbling in web design and have built several websites including one for a paying client using Webflow. This has given me a great understanding of the physyical structure of HTML but seen through a graphical user interface. This has helped me a lot in the first few weeks at CoderAcademy as I already have a strong understanding of HTML elements, flex-box, and styling.</p>
+    </div>
+
+    
   </section>
 
 );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 
 const HomePage = () => (
     <div className="main-info" id="home">
-        <h1>Duncan Bean</h1>
+        <h2>About Me</h2>
         <h4>Junior Web Developer</h4>
-        <p>I love user-centric design and all building things with code</p>
+        <p>I am a budding web developer, eager to dive into the world of creating amazing online experiences. With a hunger for learning and a keen eye for detail, I'm here to share the web projects I've been working on as I grow my skills.</p>
+        <p>In my previous careers I have been an iterior designer, teacher, and 3D digital artist.</p>
+        <p>In 2015-16 I was studying 3D modelling and animation at AIE. During this time I did a deep dive into character rigging and taught myself how to use Python within Maya for this purposed. It was great fun writing code to build machines that did the tedious work for me. My high point was creating an automated character rigging interface that built skeletons and applied control rigs with the push of a button.</p>
+        <p>Over the past few years I have been dabbling in web design and have built several websites including one for a paying client using Webflow. This has given me a great understanding of the physyical structure of HTML but seen through a graphical user interface. This has helped me a lot in the first few weeks at CoderAcademy as I already have a strong understanding of HTML elements, flex-box, and styling.</p>
     </div>
 );
 
