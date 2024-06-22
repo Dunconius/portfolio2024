838c76d6309df8405ee70fd9832306ee3a788011
Dunconius <duncanbean84@gmail.com>
2024-06-22 17:44:03 +1000

    fix: experience blurbs updated

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -5,40 +5,23 @@ const Experience = () => (
         <div className="work-history">
             <h3>Teacher - Interior Design</h3>
             <h4>Canberra Institute of Technology</h4>
-            <ul>
-                <li>Developing and delivering educational modules on design principles, 3D modelling, and architectural drafting.</li>
-                <li>Creating and implementing engaging learning activities for
-                diverse design topics.</li>
-                <li>Conducting instructional sessions on design presentations, 3D
-                rendering, and technical documentation.</li>
-                <li>Adapting teaching methods to facilitate understanding of
-                complex concepts and tools.</li>
-            </ul>
+            <p>
+            In my role as a teacher, I develop and delivere educational content on a diverse range of topics covering design principles, 3D modeling, and architectural drafting. I create and implement engaging learning activities to ensure that lessons are both informative and interactive. I provide students with the skills and knowledge they needed to excel, adapting my teaching methods to facilitate the understanding of complex concepts and tools, tailoring my approach to meet the diverse needs of my students.
+            </p>
         </div>
         <div className="work-history">
             <h3>3D Artist</h3>
             <h4>Self Employed</h4>
-            <ul>
-                <li>Providing high-quality 3D visuals for client presentations and
-                marketing.</li>
-                <li>Producing 3D models, rendered images, and animations based
-                on architectural drawings.</li>
-                <li>Consulting with clients to understand briefs and deliver desired
-                outcomes.</li>
-            </ul>
+            <p>
+            In my freelance work as a 3D architectural visualisation artist, I consistently deliver high-quality 3D visuals for client presentations and marketing purposes. This involves producing detailed 3D models, rendered images, and animations based on architectural drawings, ensuring accuracy and visual appeal. I work closely with my clients to thoroughly understand their briefs and objectives, allowing me to tailor my work to their desired outcomes. My freelance experience has honed my ability to work independently, manage client expectations, and produce visually stunning results that meet or exceed client expectations.
+            </p>
         </div>
         <div className="work-history">
             <h3>Senior Interior Designer</h3>
             <h4>IA Design</h4>
-            <ul>
-                <li>Collaborated with team members to design and deliver office
-                interior design projects for private and government clients.</li>
-                <li>Consulted with clients to confirm detailed design briefs.</li>
-                <li>Liaised with external contractors to ensure coordination of
-                various trades, services, and consultants.</li>
-                <li>Developed, implemented, and managed architectural drafting
-                and documentation standards and practices.</li>
-            </ul>
+            <p>
+            During my time as an interior designer, I collaborated closely with team members to design and deliver office interior design projects for a diverse range of clients, including private entities and government organisations. This involved consulting extensively with clients to confirm detailed design briefs and ensure that our designs met their specific needs. I also liaised with external contractors to coordinate various trades, services, and consultants, facilitating the successful execution of our designs. In my role as CAD Manager I developed, implemented, and managed architectural drafting and documentation standards and practices, ensuring consistency and efficiency in our project workflows. My experience in interior design has equipped me with the skills to work collaboratively, communicate effectively with clients and stakeholders, and deliver innovative and functional design solutions.
+            </p>
         </div>
     </section>
 );
