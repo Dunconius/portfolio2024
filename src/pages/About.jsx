b2888cf5381cb69821e34e3303007c4902f6e8e8
Dunconius <duncanbean84@gmail.com>
2024-10-01 12:15:11 +1000

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,12 +7,12 @@ const About = () => (
     <div className="info-content-wrapper">
       <p>With a strong background in interior design and 3D animation, I bring a unique perspective to web development. My previous experience has given me a deep understanding of user-centric design and I incorporate this into everything I do. I love untangling the brief and solving the puzzles of the design. I am passionate about learning and enjoy crafting intuitive and visually appealing web solutions.</p>
     </div>
-    <div>
+    {/* <div>
       <h3><em>
         "A house is a machine for living in. <br />An armchair is a machine for sitting in."
         </em></h3>
       <h5>Le Courbusier</h5>
-    </div>
+    </div> */}
 
     
   </section>
